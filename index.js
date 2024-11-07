4e5abf051bd132cada90f26a9c76d4ad913248eb
John Willwerth <arw1317@protonmail.com>
2024-11-07 15:35:42 -0500

    1

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from 'express';
-import Hello from "./Hello.js"; // import Hello from Hello.js
-const app = express() // create new express instance
-Hello(app) // pass app reference to Hello
-app.listen(4000) // listen to http://localhost:4000
+import Hello from "./Hello.js";     // import Hello from Hello.js
+import Lab5 from "./Lab5/index.js"; // import Lab5
+const app = express()               // create new express instance
+app.use(express.json());
+Lab5(app);                          // pass reference to express module
+Hello(app);                         // pass app reference to Hello
+app.listen(4000)                    // listen to http://localhost:4000
