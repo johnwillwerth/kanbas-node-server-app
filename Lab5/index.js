3925d31d4ded50e6e545a6716d790702a9812642
John Willwerth <arw1317@protonmail.com>
2024-11-08 13:46:24 -0500

    3

--- Lab5/index.js
+++ Lab5/index.js
@@ -2,6 +2,7 @@ import PathParameters from "./PathParameters.js";
 import QueryParameters from "./QueryParameters.js";
 import WorkingWithObjects from "./WorkingWithObjects.js";
 import Module from "./Module.js";
+import WorkingWithArrays from "./WorkingWithArrays.js";
 export default function Lab5(app) {        // accept app reference to express module
   app.get("/lab5/welcome", (req, res) => { // create route to welcome users to Lab 5.
   res.send("Welcome to Lab 5");            // Here we are using the new arrow function syntax
@@ -10,4 +11,5 @@ export default function Lab5(app) {        // accept app reference to express mo
   QueryParameters(app);
   WorkingWithObjects(app);
   Module(app);
+  WorkingWithArrays(app);
 };
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import Hello from "./Hello.js";      // import Hello from Hello.js
 import Lab5 from "./Lab5/index.js";  // import Lab5
+import cors from "cors";
 const app = express()                // create new express instance
+app.use(cors());
 Lab5(app);                           // pass reference to express module
 Hello(app);                          // pass app reference to Hello
 app.listen(process.env.PORT || 4000) // listen to http://localhost:4000
