887efa91825ed84f9002a35f1b2f79db428e5ae7
John Willwerth <arw1317@protonmail.com>
2024-11-09 21:29:59 -0500

    lab5-complete

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -13,13 +13,14 @@ export default function WorkingWithArrays(app) {
     res.json(todos);
   });
   app.get("/lab5/todos/create", (req, res) => {
-    const newTodo = {
-      id: new Date().getTime(),
-      title: "New Task",
-      completed: false,
-    };
+    const newTodo = { id: new Date().getTime(), title: "New Task", completed: false };
     todos.push(newTodo);
     res.json(todos);
+  });
+  app.post("/lab5/todos", (req, res) => {
+    const newTodo = { ...req.body, id: new Date().getTime() };
+    todos.push(newTodo);
+    res.json(newTodo);
   });
   app.get("/lab5/todos/:id", (req, res) => {
     const { id } = req.params;
@@ -28,9 +29,32 @@ export default function WorkingWithArrays(app) {
   });
   app.get("/lab5/todos/:id/delete", (req, res) => {
     const { id } = req.params;
-    const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
-    todos.splice(todoIndex, 1);
+    const todo = todos.find((t) => t.id === parseInt(id));
+    const todoIndex = todos.indexOf(todo);
+    if (todoIndex !== -1) {
+      todos.splice(todoIndex, 1);
+    }
     res.json(todos);
+  });
+  app.delete("/lab5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+    if (todoIndex === -1) {
+      res.status(404).json({ message: `Unable to delete Todo with ID ${id}` });
+      return;
+    }
+    todos.splice(todoIndex, 1);
+    res.sendStatus(200);
+  });
+  app.put("/lab5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+    if (todoIndex === -1) {
+      res.status(404).json({ message: `Unable to update Todo with ID ${id}` });
+      return;
+    }
+    todos = todos.map((t) => { `...` });
+    res.sendStatus(200);
   });
   app.get("/lab5/todos/:id/title/:title", (req, res) => {
     const { id, title } = req.params;
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import Lab5 from "./Lab5/index.js";  // import Lab5
 import cors from "cors";
 const app = express()                // create new express instance
 app.use(cors());
+app.use(express.json());
 Lab5(app);                           // pass reference to express module
 Hello(app);                          // pass app reference to Hello
 app.listen(process.env.PORT || 4000) // listen to http://localhost:4000
