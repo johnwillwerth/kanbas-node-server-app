f7a55580f7301c0615cc9ccf86e2b5fbb40a90a8
John Willwerth <arw1317@protonmail.com>
2024-11-10 21:17:54 -0500

    1

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -1,29 +1,50 @@
 import * as dao from "./dao.js";
-let currentUser = null;
 
 export default function UserRoutes(app) {
   const createUser = (req, res) => { };
   const deleteUser = (req, res) => { };
   const findAllUsers = (req, res) => { };
   const findUserById = (req, res) => { };
-  const updateUser = (req, res) => { };
+  const updateUser = (req, res) => {
+    const userId = req.params.userId;
+    const userUpdates = req.body;
+    dao.updateUser(userId, userUpdates);
+    const currentUser = dao.findUserById(userId);
+    req.session["currentUser"] = currentUser;
+    res.json(currentUser);
+  };
   const signup = (req, res) => {
     const user = dao.findUserByUsername(req.body.username);
     if (user) {
-      res.status(400).json(
-        { message: "Username already in use" });
+      res.status(400).json({ message: "Username already taken" });
       return;
     }
-    currentUser = dao.createUser(req.body);
+    const currentUser = dao.createUser(req.body);
+    req.session["currentUser"] = currentUser;
     res.json(currentUser);
   };
   const signin = (req, res) => {
     const { username, password } = req.body;
-    currentUser = dao.findUserByCredentials(username, password);
+    const currentUser = dao.findUserByCredentials(username, password);
+    if (currentUser) {
+      req.session["currentUser"] = currentUser;
+      res.json(currentUser);
+    } else {
+      res.status(401).json({ message: "Unable to login. Try again later." });
+    }
+  };
+  const profile = (req, res) => {
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) {
+      res.sendStatus(401);
+      return;
+    }
     res.json(currentUser);
   };
-  const signout = (req, res) => { };
-  const profile = (req, res) => { };
+  const signout = (req, res) => {
+    req.session.destroy();
+    res.sendStatus(200);
+  };
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
   app.get("/api/users/:userId", findUserById);
--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 import express from 'express';
-import Hello from "./Hello.js";      // import Hello from Hello.js
-import Lab5 from "./Lab5/index.js";  // import Lab5
+import Hello from "./Hello.js"; 
+import Lab5 from "./Lab5/index.js"; 
 import cors from "cors";
 import UserRoutes from "./Kanbas/Users/routes.js";
-const app = express()                // create new express instance
-app.use(cors());
+import "dotenv/config";
+import session from 'express-session';                          // import new server session library
+const app = express()                                           // create new express instance
+app.use(
+  cors({                                                        // configure cors first
+    credentials: true,                                          // support cookies
+    origin: process.env.NETLIFY_URL || "http://localhost:3000", // use different front end URL in dev
+  })                                                            // and in production
+);
+
 app.use(express.json());
+
+const sessionOptions = {                                        // default session options
+  secret: process.env.SESSION_SECRET || "kanbas",       
+  resave: false,                                       
+  saveUninitialized: false,                                  
+};
+if (process.env.NODE_ENV !== "development") {                   // in production
+  sessionOptions.proxy = true;                                  // turn on proxy support
+  sessionOptions.cookie = {                                     // configure cookies for remote server
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
 UserRoutes(app);
-Lab5(app);                           // pass reference to express module
-Hello(app);                          // pass app reference to Hello
-app.listen(process.env.PORT || 4000) // listen to http://localhost:4000
+Lab5(app);                                                      // pass reference to express module
+Hello(app);                                                     // pass app reference to Hello
+app.listen(process.env.PORT || 4000)                            // listen to http://localhost:4000
